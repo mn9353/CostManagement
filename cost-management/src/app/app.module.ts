b0f38a5bea1d846fcc2cf55ebb8689f4b2b7ae30
devojeet02 <devojeet.modak@acumant.com>
2026-05-19 15:12:45 +0530

    Component Structurization and frontend augmentation

--- cost-management/src/app/app.module.ts
+++ cost-management/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './features/app-header/app-header.component';
+import { TopNavComponent } from './features/top-nav/top-nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    TopNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
